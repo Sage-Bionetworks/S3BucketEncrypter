9cd994d3d114b86e11a129b5fbf61907c6ec3152
bhoff <bruce.hoff@sagebase.org>
2019-01-17 13:44:00 -0800

    added an integration test

--- S3BucketEncrypter.py
+++ S3BucketEncrypter.py
@@ -10,9 +10,53 @@ import os
 import argparse
 import boto3
 
-def s3Encrypt(bucketName, key):
+def s3Encrypt(bucketName, key, s3Client):
+    acl = s3Client.get_object_acl(Bucket=bucketName, Key=key)
+    acl.pop('ResponseMetadata', None)
     s3Client.copy_object(CopySource={'Bucket': bucketName, 'Key': key}, Bucket=bucketName, Key=key, ServerSideEncryption='AES256')
+    s3Client.put_object_acl(Bucket=bucketName, Key=key, AccessControlPolicy=acl)
     
+def encryptBucket(awsKeyId, awsKeySecret, awsSessionToken, bucket, maxKeysPerBatch, startAfter, maxNumberToProcess, dryrun):
+    s3Client= boto3.client('s3', aws_access_key_id=awsKeyId, aws_secret_access_key=awsKeySecret, aws_session_token=awsSessionToken)
+    
+    counter=0
+    continuationToken=None
+    while True:
+        if continuationToken is None:
+            if startAfter is None:   
+                objs = s3Client.list_objects_v2(Bucket=bucket, MaxKeys=maxKeysPerBatch)
+            else:
+                objs = s3Client.list_objects_v2(Bucket=bucket, StartAfter=startAfter, MaxKeys=maxKeysPerBatch)
+        else:
+            objs = s3Client.list_objects_v2(Bucket=bucket, ContinuationToken=continuationToken, MaxKeys=maxKeysPerBatch)
+        contents = objs.get('Contents')
+        continuationToken = objs.get('NextContinuationToken')
+        if contents is None or len(contents)==0:
+            break
+        i = 0
+        while i<len(contents) and (maxNumberToProcess is None or counter<maxNumberToProcess):
+            key = contents[i]['Key']
+            firstKey=key
+            try:
+                objectMeta = s3Client.head_object(Bucket=bucket, Key=key)
+                sse = objectMeta.get('ServerSideEncryption')
+                if sse == 'AES256':
+                    print(key+'\tServerSideEncryption: '+sse+"\tNo encryption necessary")
+                else:
+                    if not dryrun:
+                        s3Encrypt(bucket, key, s3Client)
+                    print(key+'\t encrypted using ServerSideEncryption')
+            except Exception as e:
+                print(key+'\t'+str(e))
+            i = i + 1
+            counter = counter + 1
+        if maxNumberToProcess is not None and counter>=maxNumberToProcess:
+            break
+        if continuationToken is None:
+            break
+
+    print("Processed "+str(counter)+" files.")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--bucket", required=True, help="bucket name")
@@ -21,40 +65,6 @@ if __name__ == '__main__':
     parser.add_argument("-awstoken", "--awsSessionToken", required=False, help="AWS MFA Session Token")
     parser.add_argument("-m", "--maxNumberToProcess", type=int, help="Maximum number of files to process", required=False)
     parser.add_argument("-d", "--dryrun", action='store_true', help="dry run")
-    parser.add_argument("-s", "--startKey", required=False, help="first key to examine")
+    parser.add_argument("-s", "--startAfter", required=False, help="the last key successfully processed")
     args = parser.parse_args()
-
-    s3Client= boto3.client('s3', aws_access_key_id=args.awsKeyId, aws_secret_access_key=args.awsKeySecret, aws_session_token=args.awsSessionToken)
-    
-    counter=0
-    firstKey=args.startKey
-    while True:
-        if firstKey is None:
-            objs = s3Client.list_objects(Bucket=args.bucket)
-        else:
-            objs = s3Client.list_objects(Bucket=args.bucket, Marker=firstKey)
-        contents = objs.get('Contents')
-        if contents is None or len(contents)==0:
-            break
-        i = 0
-        while i<len(contents) and (args.maxNumberToProcess is None or counter<args.maxNumberToProcess):
-            key = contents[i]['Key']
-            firstKey=key
-            try:
-                objectMeta = s3Client.head_object(Bucket=args.bucket, Key=key)
-                sse = objectMeta.get('ServerSideEncryption')
-                if sse == 'AES256':
-                    print(key+'\tServerSideEncryption: '+sse+"\tNo encryption necessary")
-                else:
-                    if not args.dryrun:
-                        s3Encrypt(args.bucket, key)
-                    print(key+'\t encrypted using ServerSideEncryption')
-            except Exception as e:
-                print(key+'\t'+str(e))
-            i = i + 1
-            counter = counter + 1
-        if args.maxNumberToProcess is not None and counter>=args.maxNumberToProcess:
-            break
-
-    print("Processed "+str(counter)+" files.")
-
+    encryptBucket(args.awsKeyId, args.awsKeySecret, args.awsSessionToken, args.bucket, 200, args.startAfter, args.maxNumberToProcess, args.dryrun)
---
+++ S3BucketEncrypterTest.py
@@ -0,0 +1,78 @@
+'''
+Tests S3BucketEncrypter
+
+To run
+
+python S3BucketEncrypterTest.py -b plfm5082-upstream.sagebase.org \
+-skid xxx -sksec xxxx -sawstoken xxxxx \
+-ekid xxx -eksec xxxx -eawstoken xxxxx
+
+
+@author: bhoff
+'''
+import json
+import os
+import argparse
+import boto3
+import random, string
+from S3BucketEncrypter import encryptBucket
+
+
+def randomword(length):
+   letters = string.ascii_lowercase
+   return ''.join(random.choice(letters) for i in range(length))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bucket", required=True, help="bucket name")
+    parser.add_argument("-skid", "--setup_awsKeyId", required=True, help="AWS Key ID for account which creates the files")
+    parser.add_argument("-sksec", "--setup_awsKeySecret", required=True, help="AWS Key Secret for account which creates the files")
+    parser.add_argument("-sawstoken", "--setup_awsSessionToken", required=False, help="AWS MFA Session Token for account which creates the files")
+    parser.add_argument("-ekid", "--encrypt_awsKeyId", required=True, help="AWS Key ID for the account which encrypts the files")
+    parser.add_argument("-eksec", "--encrypt_awsKeySecret", required=True, help="AWS Key Secret for the account which encrypts the files")
+    parser.add_argument("-eawstoken", "--encrypt_awsSessionToken", required=False, help="AWS MFA Session Token for the account which encrypts the files")
+    parser.add_argument("-m", "--maxNumberToProcess", type=int, help="Maximum number of files to process", required=False)
+    parser.add_argument("-d", "--dryrun", action='store_true', help="dry run")
+    parser.add_argument("-s", "--startAfter", required=False, help="the last key successfully processed")
+    args = parser.parse_args()
+    
+    s3Client= boto3.client('s3', aws_access_key_id=args.setup_awsKeyId, aws_secret_access_key=args.setup_awsKeySecret, aws_session_token=args.setup_awsSessionToken)
+
+    # available operations for the client:
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html
+    
+    # create some files with metadata
+    keysToDelete = []
+    acls = {}
+    for i in range(20):
+        name = 'file_'+str(i)+'.txt'
+        body = bytearray(randomword(1000), 'utf-8')
+        metadata={"meta-name":"meta-"+name}
+        s3Client.put_object(Bucket=args.bucket, Body=body, Key=name, ACL='public-read-write', Metadata=metadata)
+        keysToDelete.append(name)
+        acls[name]=s3Client.get_object_acl(Bucket=args.bucket, Key=name)
+        
+    # run S3BucketEncrypter on bucket
+    # Note: user encrypting the files is other than the user who created them
+    encryptBucket(args.encrypt_awsKeyId, args.encrypt_awsKeySecret, args.encrypt_awsSessionToken, args.bucket, 5, startAfter=None, maxNumberToProcess=args.maxNumberToProcess, dryrun=args.dryrun)
+    
+    for key in keysToDelete:
+        print("Checking "+key)
+        obj = s3Client.get_object(Bucket=args.bucket, Key=key)
+        # verify file is encrypted
+        assert obj.get('ServerSideEncryption')=='AES256', key+" is not encrypted"
+        # verify that metadata is preserved
+        expectedMeta={"meta-name":"meta-"+key}
+        assert obj.get('Metadata')==expectedMeta, "Unxpected metadata: "+obj.get('Metadata')
+        # verify that permissions and ownership are preserved
+        currentAcl = s3Client.get_object_acl(Bucket=args.bucket, Key=key)
+        assert currentAcl['Owner']==acls[key]['Owner'], "Owner has changed from\n"+json.dumps(acls[key]['Owner'])+"\nto\n"+json.dumps(currentAcl['Owner'])
+        assert currentAcl['Grants']==acls[key]['Grants'], "Grants have changed from\n"+json.dumps(acls[key]['Grants'])+"\nto\n"+json.dumps(currentAcl['Grants'])
+        
+    print("All files pass checks.")
+    
+    # tear down
+    for key in keysToDelete:
+        s3Client.delete_object(Bucket=args.bucket, Key=key)
+    print("Clean up is done.")
+    
